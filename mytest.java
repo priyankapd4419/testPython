f168191f85c2789651e064cd449f925c60b36803
priyanka <priyanka.priya.pd@gmail.com>
2017-08-30 21:35:27 +0530

    this is my first code commit

---
+++ mytest.java
@@ -0,0 +1,18 @@
+package deadlocks;
+
+class Account {
+    private int balance = 10000;
+
+    public void deposit(int amount) {
+        balance += amount;
+    }
+
+    public void withdraw(int amount) {
+        balance -= amount;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public static void transfer(Account acc1, Account acc2, int amount) {:wq
